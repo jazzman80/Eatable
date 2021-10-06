cf2815ba9bc19ff840b661276095bd36198a50e9
Ivan <jazzman.ain@gmail.com>
2021-10-06 22:04:08 +0300

    Objects manager

---
+++ Assets/Scripts/ObjectsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] objectsPool;
+
+    public void ObjectsOff()
+    {
+        foreach (GameObject gameObject in objectsPool) gameObject.SetActive(false);
+    }
+
+    public void ObjectsOn()
+    {
+        foreach (GameObject gameObject in objectsPool) gameObject.SetActive(true);
+    }
+}
+
