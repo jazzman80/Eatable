bfd159e7126dfa3583ba26beb7a1e357c2bc13bc
Ivan <jazzman.ain@gmail.com>
2021-10-06 14:24:08 +0300

    Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Vector2 dragStartPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        dragStartPosition = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (dragStartPosition.x >= eventData.position.x) Debug.Log("Swipe left");
+        else if (dragStartPosition.x < eventData.position.x) Debug.Log("Swipe right");
+    }
+}
