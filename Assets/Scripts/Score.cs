c3023079b0c6556cdef2fe6c3a305056f638b475
Ivan <jazzman.ain@gmail.com>
2021-10-06 17:42:01 +0300

    Подсчёт и отображение очков

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    int score;
+
+    [SerializeField] TextMeshProUGUI scoreUI;
+
+    public void AddScore()
+    {
+        score += 10;
+
+        scoreUI.text = score.ToString();
+    }
+}
