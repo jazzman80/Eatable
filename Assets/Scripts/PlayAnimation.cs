2e5464f91a3175ccb012dfe8a1d7e79bb558f8ef
Ivan <jazzman.ain@gmail.com>
2021-10-07 22:06:43 +0300

    Анимация убирания карточек

---
+++ Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] string animationName;
+
+    public void ReplayAnimation()
+    {
+        animator.Play(animationName, -1, 0.0f);
+    }
+}
